b6e33a8f64507b353ff19ebb870b9c43cf7ea89b
Epicgamer30001 <alvinpark7412@gmail.com>
2026-07-14 16:33:50 -0700

    bo

--- button.py
+++ button.py
@@ -6,17 +6,29 @@ class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
 
+        self.button_is_checked = True   #initial state
+
         self.setWindowTitle("My App")
         
-        button = QPushButton("Press me!")
-        button.setCheckable(True)
-        button.clicked.connect(self.button_cb)
+        self.button = QPushButton("Press me!")
+        self.button.setCheckable(True)        #light switch = True 
+        self.button.clicked.connect(self.button_cb)  #define callback
+ 
+        self.button.setChecked(self.button_is_checked)   #set inital state 
 
-        self.setCentralWidget(button)
+        self.setCentralWidget(self.button)        
 
     def button_cb(self):
-        print("Clicked!")
+        self.button_is_checked = self.button.isChecked()
 
+        print(self.button_is_checked)
+"""
+    def button_cb(self,checked):
+        self.button_is_checked = checked
+
+        print(self.button_is_checked)"""
+    
+        
 
 app = QApplication(sys.argv)
 
